a21d077e7f1190a1193e57dedc1a77c58ef9d426
Sam Bostock <sam.bostock@shopify.com>
2021-04-16 20:17:10 -0400

    Extract common tests into IntegrationBehaviour
    By extracting the integration tests into a module, we can easily ensure
    they all share features.

--- test/integration/resque_test.rb
+++ test/integration/resque_test.rb
@@ -3,59 +3,18 @@
 require "test_helper"
 
 require_relative "../support/jobs"
+require_relative "integration_behaviour"
 
 class ResqueIntegrationTest < ActiveSupport::TestCase
-  setup do
-    @original_adapter = ActiveJob::Base.queue_adapter
-    ActiveJob::Base.queue_adapter = :resque
-  end
-
-  teardown do
-    ActiveJob::Base.queue_adapter = @original_adapter
-  end
-
-  test "interrupts the job" do
-    IterationJob.perform_later
-
-    start_resque_and_wait
-
-    assert_equal 1, queue_size
-    job_args = jobs_in_queue.first.fetch("args")
-    assert_equal 0, job_args.dig(0, "cursor_position")
-    assert_equal 1, job_args.dig(0, "times_interrupted")
-
-    start_resque_and_wait
-
-    assert_equal 1, queue_size
-    job_args = jobs_in_queue.first.fetch("args")
-    assert_equal 2, job_args.dig(0, "cursor_position")
-    assert_equal 2, job_args.dig(0, "times_interrupted")
-
-    TerminateJob.perform_later
-    start_resque_and_wait
-
-    assert_equal 0, queue_size
-  end
-
-  test "unserializable corruption is prevented" do
-    # Resque serializes cursors as JSON, but not all objects are serializable.
-    #     time   = Time.at(0).utc   # => 1970-01-01 00:00:00 UTC
-    #     json   = JSON.dump(time)  # => "\"1970-01-01 00:00:00 UTC\""
-    #     string = JSON.parse(json) # => "1970-01-01 00:00:00 UTC"
-    # We serialized a Time, but it was deserialized as a String.
-    TimeCursorJob.perform_later
-    TerminateJob.perform_later
-    start_resque_and_wait
-
-    assert_equal(
-      JobIteration::Iteration::CursorError.name,
-      failed_job_error_class_name,
-    )
-  end
+  include IntegrationBehaviour
 
   private
 
-  def start_resque_and_wait
+  def queue_adapter
+    :resque
+  end
+
+  def start_worker_and_wait
     pid = nil
     Dir.chdir("test/support/resque") do
       pid = spawn(resque_env, "bundle exec rake resque:work")
@@ -76,6 +35,10 @@ class ResqueIntegrationTest < ActiveSupport::TestCase
 
   def queue_size
     Resque.queue_sizes.fetch("default")
+  end
+
+  def job_args
+    jobs_in_queue.first.fetch("args")
   end
 
   def jobs_in_queue

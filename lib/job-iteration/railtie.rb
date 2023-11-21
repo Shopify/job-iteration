cbb89f8271006dc1b4c15cc5944571bea7710748
Sam Bostock <sam.bostock@shopify.com>
2023-11-21 03:34:41 -0500

    Add `Deprecation`

--- lib/job-iteration/railtie.rb
+++ lib/job-iteration/railtie.rb
@@ -4,5 +4,8 @@ return unless defined?(Rails::Railtie)
 
 module JobIteration
   class Railtie < Rails::Railtie
+    initializer "job_iteration.register_deprecator" do |app|
+      app.deprecators[:job_iteration] = JobIteration::Deprecation
+    end
   end
 end

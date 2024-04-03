0670494771ca554d24a98a687e472d2e478c514e
Sander Verdonschot <sander.verdonschot@shopify.com>
2024-04-03 11:50:35 -0400

    Change Rubocop config test to CI matrix test

---
+++ test/ci_matrix_test.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "yaml"
+
+module JobIteration
+  class CiMatrixTest < ActiveSupport::TestCase
+    test "Required Ruby version in gemspec matches oldest Ruby in CI matrix" do
+      assert_equal(
+        oldest_ruby_in_matrix,
+        required_ruby_version,
+        "Required Ruby version in gemspec does not match oldest Ruby in CI matrix",
+      )
+    end
+
+    test "Dev Ruby version matches the newest Ruby in CI matrix" do
+      assert_equal(
+        newest_ruby_in_matrix,
+        ruby_version_for_development,
+        "Development does not use the newest Ruby in CI matrix",
+      )
+    end
+
+    private
+
+    def oldest_ruby_in_matrix
+      ruby_versions_in_matrix.min
+    end
+
+    def newest_ruby_in_matrix
+      ruby_versions_in_matrix.max
+    end
+
+    def ruby_versions_in_matrix
+      YAML
+        .load_file(".github/workflows/ci.yml")
+        .dig("jobs", "build", "strategy", "matrix", "ruby")
+        .tap { |ruby_versions| refute_nil(ruby_versions, "Ruby versions not found in CI matrix") }
+        .map { |ruby_version| Gem::Version.new(ruby_version) }
+    end
+
+    def ruby_version_for_development
+      File
+        .read(".ruby-version")
+        .strip
+        .then { |ruby_version| Gem::Version.new(ruby_version) }
+    end
+
+    def required_ruby_version
+      Gem
+        .loaded_specs
+        .fetch("job-iteration")
+        .required_ruby_version
+        .to_s[/(?<=>= )\d+\.\d+/]
+        .then { |ruby_version| Gem::Version.new(ruby_version) }
+    end
+  end
+end
